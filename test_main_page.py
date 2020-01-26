6f8c89961460bae9a500a6aca9c346da4c378dd1
WicketGeneral <danketolog@gmail.com>
2020-01-26 17:47:35 +0300

    Add switch to LoginPage

--- test_main_page.py
+++ test_main_page.py
@@ -18,6 +18,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)    # создаём объект страницы логина
+    login_page.should_be_login_page()    # проверяем, что попали именно на страницу логина
 
 
 def test_guest_should_see_login_link(browser):
