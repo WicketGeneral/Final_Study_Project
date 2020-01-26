1c42407c8cf32806e67a754d343a99d01e0ba4d0
WicketGeneral <danketolog@gmail.com>
2020-01-26 13:16:12 +0300

    test_main_page refactored

--- test_main_page.py
+++ test_main_page.py
@@ -1,22 +1,12 @@
-import pytest
+'''import pytest
 import math
 import time
-from selenium import webdriver
+from selenium import webdriver'''
+from .pages.main_page import MainPage
 
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    browser.implicitly_wait(5)
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
 
 def test_guest_can_go_to_login_page(browser):
-   link = "http://selenium1py.pythonanywhere.com/"
-   browser.get(link)
-   go_to_login_page(browser)
-
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
